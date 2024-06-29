546cc99ef4d3ef58f6d77654ea44499b4e6cdc5d
Paulo <paulofcoelho20@gmail.com>
2024-06-29 10:41:30 -0700

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss"; // Importe o Tailwind CSS se estiver usando
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
   plugins: [
     react(),
     tailwindcss("./tailwind.config.js"), // Configure o Tailwind CSS conforme necessário
