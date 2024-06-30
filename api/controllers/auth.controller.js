3f1a720bc7dc4f335a3c3a049a1a8be52cc0a49a
Paulo <paulofcoelho20@gmail.com>
2024-06-29 21:13:50 -0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password, admin } = req.body;
@@ -11,10 +12,27 @@ export const signup = async (req, res, next) => {
     password: hashedPassword,
     admin,
   });
-
   try {
     await newUser.save();
     res.status(201).json({ message: "User Created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "Invalid credentials"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc; // se nao quiser passar valor do usuario, adicione o valor entras as "{valor, password: hashedPassword, ...rest}"
+    const expiryDate = new Date(Date.now() + 3600000); //1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
