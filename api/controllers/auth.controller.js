5a0365dce5ef01cc601f695dc5b95acc6d9247a5
Paulo <paulofcoelho20@gmail.com>
2024-06-30 20:45:19 -0700

    add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler(404, "Invalid credentials"));
+    if (!validUser) return next(errorHandler(404, "Invalid credentials USER"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
